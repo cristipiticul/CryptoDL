9a744af3252360cc62d3a8f2217e25b4eab14f9e
Cristian Militaru <cristipiticul@yahoo.com>
2024-03-31 12:49:28 +0300

    Latest changes

--- examples/mnist_cnn/mnist_cnn.py
+++ examples/mnist_cnn/mnist_cnn.py
@@ -1,9 +1,10 @@
 import kalypso
 import keras
+import numpy as np
 
 # load the pretrained model
-m = keras.models.load_model( 'mnistCNN.h5' )
+m = keras.models.load_model("mnistCNN.h5")
+for layer in m.layers:
+    layer.set_weights(np.array(layer.get_weights()) + 0.00000001)
 # extract the model to the weights folder
-kalypso.export_weights( m, 'weights' )
-
-
+kalypso.export_weights(m, "weights")
